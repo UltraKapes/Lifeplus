da3f809986bd275cc49a2b6e7ffb40b93b6a5a45
UltraKapes <kapes1980@hotmail.com>
2015-04-10 19:50:10 +1000

    First commit

---
+++ index.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+    
+        <link href="css/indexstyle.css" rel="stylesheet">
+      
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+		
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="css/lightbox.css" rel="stylesheet" />
+		<title id="dynamicTitle">Life+</title>
+		<script type="text/javascript">
+		$(window).scroll(function() {    
+   		 var scroll = $(window).scrollTop();
+
+    	 //>=, not <=
+				if (scroll >= 100) {
+        //clearHeader, not clearheader - caps H
+        $("#header").addClass("fixednav");
+    }else{$("#header").removeClass("fixednav"); }
+});
+		</script>
+		
+	</head>
+	<body >
+	<div class="col-xs-12"id="header">
+		    <div id="logo">
+				<img class="setImage" src="images/logo.gif">
+			</div>
+			<div id="nav">
+				<ul>
+				
+					<li><a href="shop.php">Explore</a></li>
+					<li><a href="#">Login</a></li>
+				
+				</ul>
+			</div>
+		
+	    </div>
+	<div class="col-xs-12" id="body-container">
+	    <div class="fixed-bg bg-1">
+	    	
+	    </div>
+	    <div class="fixed-bg bg-2 header">
+			<div class ="header-content">
+			<h1>Some Title</h1>
+			</div>
+		</div>
+	    <div class="scrolling-bg bg-color1">
+			<div class="body-content">Content</div>
+	    </div>
+	    <div class="fixed-bg bg-3 header" >
+	   		<div class="header-content">
+	   		<h1>Some Title 2</h1>
+	   		</div>
+		</div>
+	    <div class="scrolling-bg bg-color1">
+	    	<div class="body-content">Content</div>
+	    </div>
+	    <div class="fixed-bg bg-4 header">
+	    	<div class="header-content">
+	    		<h1>Some Title 3</h1>
+	    	</div>
+		</div>
+	    <div class="scrolling-bg bg-color1">
+	    	<div class="body-content">Content</div>
+	    </div>
+   	</div>
+	
+
+	
+
+	</body>
+ 
+</html>
+
+
+
---
+++ shop.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Life+ Shop</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/shop.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div   class="navbar navbar-fixed-top" role="navigation">
+            <div id="topnavbar" class="container">
+                <div id="logodiv" class="col-md-2" class="navbar-header">
+                    <button type="button" class="navbar-toggle"
+                    data-toggle="collapse"
+                    data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button> <a href="index.php" class="navbar-brand">
+                    <span ><img src="images/LogoBlack.gif"></span>
+                    </a>
+                </div>
+                <div id="navoptions" class="pull-right">
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Shop</a></li>
+                            <li><a href="#">Reviews</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">FAQ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div id="mainpage">
+                <div class="row">
+                    <div class="col-md-3 bordercheck"><h3>Shop Veritcal Navbar</h3>
+                     <ul>
+                            <li><a href="#">Category 1</a></li>
+                            <li><a href="#">Category 2</a></li>
+                            <li><a href="#">Category 3</a></li>
+                            <li><a href="#">Category 4</a></li>
+                            <li><a href="#">Category 5</a></li>
+                        </ul>
+                    </div>
+                    <div id="products"class="col-md-9 bordercheck">
+                        <div class="row">
+                            <div id="carousel" class="carousel slide" data-ride="carousel" >
+                                <!-- Menu -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel" data-slide-to="1"></li>
+                                    <li data-target="#carousel" data-slide-to="2"></li>
+                                </ol>
+                                <!-- Items -->
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="http://lorempixel.com/1500/600/abstract/1" alt="Slide 1" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="http://lorempixel.com/1500/600/abstract/2" alt="Slide 2" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="http://lorempixel.com/1500/600/abstract/3" alt="Slide 3" />
+                                    </div>
+                                </div>
+                                <a href="#carousel" class="left carousel-control" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a href="#carousel" class="right carousel-control" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+
+                        </div>
+
+                        <div><h3>Products</h3></div>
+                        <div class="row">
+                            <div class="col-md-6 bordercheck"><h3>Product 1</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 2</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 3</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 4</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 5</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 6</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 7</h3></div>
+                            <div class="col-md-6 bordercheck"><h3>Product 8</h3></div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="footer"class="col-md-12 "><h3>Footer</h3></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div class="row">
+                                <div class="col-md-12"><h3>Shop</h3></div>
+                                
+                        </div>
+                        <div id="carousel" class="carousel slide" data-ride="carousel" >
+            <!-- Menu -->
+            <!-- <ol class="carousel-indicators">
+                                                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                                                <li data-target="#carousel" data-slide-to="1"></li>
+                                                <li data-target="#carousel" data-slide-to="2"></li>
+                                    </ol>
+            <!-- Items -->
+            <!--  <div class="carousel-inner">
+                                                <div class="item active">
+                                                            <img src="http://lorempixel.com/1500/600/abstract/1" alt="Slide 1" />
+                                                </div>
+                                                <div class="item">
+                                                            <img src="http://lorempixel.com/1500/600/abstract/2" alt="Slide 2" />
+                                                </div>
+                                                <div class="item">
+                                                            <img src="http://lorempixel.com/1500/600/abstract/3" alt="Slide 3" />
+                                                </div>
+                                    </div>
+                                    <a href="#carousel" class="left carousel-control" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                    </a>
+                                    <a href="#carousel" class="right carousel-control" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                    </a>
+                        </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                        <div class="col-lg-4 bg-success"><img src="http://lorempixel.com/1500/600/abstract/3" alt="Slide 3" width="50" height="50"/></div>
+                        <div class="col-lg-4 bg-danger">2</div>
+                        <div class="col-lg-4 bg-success">3</div>
+            </div>
+</div> -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script>
+                            $(document).ready(function(){
+                                console.log($('#topnavbar').height());
+                                $('#mainpage').css('padding-top', $('#topnavbar').height() + 10);
+                                $(window).resize(function(){
+                                    console.log($('#topnavbar').height());
+                                    $('#mainpage').css('padding-top', $('#topnavbar').height() + 10);
+                                });
+                            });
+</script>
+<script>
+        $('.carousel').carousel({
+        interval: 10000;
+        });
+</script>
+</body>
+</html>
